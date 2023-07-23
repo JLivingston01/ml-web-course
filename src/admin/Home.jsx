0837e74f172821eedb9d2754c446b8bdecb9a4c3
JLivingston01 <jason.livingston@caa.columbia.edu>
2023-07-22 20:32:14 -0400

    file struct

---
+++ src/admin/Home.jsx
@@ -0,0 +1,20 @@
+import '../App.css';
+import React from 'react';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                <p>
+                    Welcome to a professional course in Machine Learning.
+                </p>
+                </header>
+            </div>
+            
+        );
+    }
+
+}
+
+export default Home;
